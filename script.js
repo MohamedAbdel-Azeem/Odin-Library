bc7700902d63d1682f830437ebb97a04e0386010
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2023-12-27 15:33:32 +0200

    Refactor code for adding books and styling improvements

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Model and View Generator
 const myLibrary = [];
 
 function Book(Title,Author,Year,isRead) {  // the constructor...
@@ -7,24 +8,19 @@ function Book(Title,Author,Year,isRead) {  // the constructor...
     this.isRead = isRead;
 }
 
-Book.prototype.addBookToLibrary = function(){
-    myLibrary.push(this);
+function addBookToLibrary(Title,Author,Year,isRead){
+    const book = new Book(Title,Author,Year,isRead);
+    myLibrary.push(book);
 }
 
 
 function generateDummyBooks(){
-    const b1 = new Book("The Hobbit","J.R.R. Tolkien",1937,true);
-    const b2 = new Book("The Fellowship of the Ring","J.R.R. Tolkien",1954,false);
-    const b3 = new Book("The Two Towers","J.R.R. Tolkien",1954,false);
-    const b4 = new Book("The Return of the King","J.R.R. Tolkien",1955,false);
-    const b5 = new Book("The Silmarillion","J.R.R. Tolkien",1977,false);
-    const b6 = new Book("The Children of Hurin","J.R.R. Tolkien",2007,false);
-    b1.addBookToLibrary();
-    b2.addBookToLibrary();
-    b3.addBookToLibrary();
-    b4.addBookToLibrary();
-    b5.addBookToLibrary();
-    b6.addBookToLibrary();
+    addBookToLibrary("The Hobbit","J.R.R. Tolkien",1937,"Yes");
+    addBookToLibrary("The Fellowship of the Ring","J.R.R. Tolkien",1954,"Yes");
+    addBookToLibrary("The Two Towers","J.R.R. Tolkien",1954,"Yes");
+    addBookToLibrary("The Return of the King","J.R.R. Tolkien",1955,"Yes");
+    addBookToLibrary("The Silmarillion","J.R.R. Tolkien",1977,"No");
+    addBookToLibrary("The Children of Hurin","J.R.R. Tolkien",2007,"No");
 }
 
 function generateLibraryDivs(){
@@ -45,3 +41,52 @@ function generateLibraryDivs(){
 
 generateDummyBooks();
 generateLibraryDivs();
+
+
+// Add Book Functionality
+const addBook = document.querySelector("#add-book");
+
+
+
+
+addBook.addEventListener("click",async function(){
+    const { value: formValues } = await Swal.fire({
+        title: "Add a Book to the Library",
+        confirmButtonText: "Add Book",
+        confirmButtonColor: "#333",
+        html: `
+          <div id="swal-inputs">
+          <div class="swal-input-element">
+          <input id="swal-input1" type="text" class="swal2-input" placeholder="Title" required>
+          </div>
+          <div class="swal-input-element">
+          <input id="swal-input2" type="text" class="swal2-input" placeholder="Author Name" required>
+          </div>
+          <div class="swal-input-element">
+          <input id="swal-input3" type="number" class="swal2-input" placeholder="Year" min=0 required>
+          </div>
+          </div>
+        `,
+        focusConfirm: false,
+        preConfirm: () => {
+          return [
+            document.getElementById("swal-input1").value,
+            document.getElementById("swal-input2").value,
+            document.getElementById("swal-input3").value,
+          ];
+        }
+      });
+      if (formValues) {
+        if (formValues[0].trim() == "" || formValues[1].trim() == "" || formValues[2] == null || formValues[2] < 0){
+            swal.fire({
+                title: "Invalid Input",
+                text: "Please enter valid input",
+                icon: "error",
+                confirmButtonText: "Ok",
+              });
+        } else {
+            addBookToLibrary(formValues[0],formValues[1],formValues[2],"No");
+            generateLibraryDivs();
+        }
+      }
+});
